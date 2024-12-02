cf0a35824c696f212691805b6299dd14f1987707
KarolynneFreire <karolynnefreire@gmail.com>
2024-12-02 11:32:04 -0300

    adicionando o banner

--- front/src/pages/Home/HeroSection/HeroSection.styles.js
+++ front/src/pages/Home/HeroSection/HeroSection.styles.js
@@ -1,31 +1,66 @@
 import styled from "styled-components";
+import cyberImg from './img/cyber.jpg'; 
 
 export const Main = styled.main`
-    max-width: 1280px;
+    max-width: 100vw; 
     width: 100%;
     height: 80vh;
     display: flex;
-    flex-direction: column;
-    /* align-items: center; */
-    justify-content: center;
+    justify-content: center; 
+    align-items: center; 
     color: var(--fiveth-color);
     font-size: 1rem;
     font-weight: 600;
-    /* border: red solid 1px; */
-`
+    background-image: url(${cyberImg}); 
+    background-size: cover; 
+    background-position: center; 
+    background-repeat: no-repeat; 
+    
+
+    @media (max-width: 768px) {
+        background-image: linear-gradient(
+            rgba(255, 255, 255, 0.4), /* Gradiente branco translúcido */
+            rgba(255, 255, 255, 0.4)
+        ), 
+        url(${cyberImg});
+    }
+`;
+
+export const Content = styled.div`
+    max-width: 1280px; 
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    @media (max-width: 768px) {
+        padding: 0 20px; 
+        text-align: center; 
+    }
+`;
 
 export const Legend = styled.p`
     color: var(--fiveth-color);
     font-size: 1rem;
     font-weight: 600;
-`
+
+    
+@media (max-width: 768px) {
+    font-size: 0.875rem; 
+}
+`;
 
 export const Title = styled.h1`
     font-weight: 900;
-    font-size: 4rem;
+    font-size: 3rem;
     color: var(--text-color);
+    margin-top: 25px;
 
     & span {
-        color: var(--fourth-color)
+        color: var(--fourth-color);
     }
-`
+
+    @media (max-width: 768px) {
+        font-size: 2rem; 
+    }
+`;
+
